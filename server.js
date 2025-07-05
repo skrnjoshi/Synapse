a519a1d96506d68e5ec9d603f7595426a506da11
Saikiran Donkana <saikirandonkana@Saikirans-MacBook-Air.local>
2025-07-05 19:39:39 +0530

    third commit

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const connectDB = require("./config/db");
 const app = express();
 
 connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes

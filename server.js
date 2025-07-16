375bf41c0bb2d1e8ec58e0b2fd0138a85be9705a
Saikiran Donkana <saikirandonkana@Saikirans-MacBook-Air.local>
2025-07-16 21:30:06 +0530

    Prepare for Render deployment - updated env and cleaned up files

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, "0.0.0.0", () =>
   console.log(`Server running on port ${PORT}`)
 );
